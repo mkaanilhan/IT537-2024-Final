032e83eadcf6859fa4bf24baa48c2eb9d0436c34
Yakup <yakup.isik@sabanciuniv.edu>
2024-07-27 15:53:34 +0300

    last version: fixed vite.config.ts

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -1,24 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-})
+        tailwindcss,
+        autoprefixer
+      ]
+    }
+  }
+});
+
